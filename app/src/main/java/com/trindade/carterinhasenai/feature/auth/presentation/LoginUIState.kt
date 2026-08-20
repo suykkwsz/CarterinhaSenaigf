605b67ef6ade97b7c37d7f6f7931f581a95dad5b
amanda campillo <amanda.baroboskin@gmail.com>
2026-08-20 11:56:05 -0300

    asd

--- app/src/main/java/com/trindade/carterinhasenai/feature/auth/presentation/LoginUIState.kt
+++ app/src/main/java/com/trindade/carterinhasenai/feature/auth/presentation/LoginUIState.kt
@@ -1,4 +1,14 @@
 package com.trindade.carterinhasenai.feature.auth.presentation
 
-class LoginUIState {
+import com.trindade.carterinhasenai.feature.auth.domain.model.UsuarioLogado
+
+data class LoginUIState (
+    val usuario: String ="",
+    val senha: String ="",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val usuarioLogado: UsuarioLogado? = null
+){
+    val loginRealizado: Boolean
+        get() = usuarioLogado != null
 }
--- app/src/main/java/com/trindade/carterinhasenai/feature/auth/presentation/LoginViewModel.kt
+++ app/src/main/java/com/trindade/carterinhasenai/feature/auth/presentation/LoginViewModel.kt
@@ -1,2 +1,82 @@
 package com.trindade.carterinhasenai.feature.auth.presentation
 
+
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.trindade.carterinhasenai.feature.auth.data.repository.FakeLoginRepositoryImpl
+import com.trindade.carterinhasenai.feature.auth.data.repository.LoginRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+
+class LoginViewModel (private val repository: LoginRepository = FakeLoginRepositoryImpl()): ViewModel(){
+    private val _uiState = MutableStateFlow(LoginUIState())
+    val uiState: StateFlow<LoginUIState> = _uiState.asStateFlow()
+
+
+
+
+    fun OnEvent(event: LoginEvent){
+        when(event){
+            is LoginEvent.OnUsuarioChange -> {
+                _uiState.update { state ->
+                    state.copy(
+                        usuario = event.value,
+                        errorMessage = null
+                    )
+                }
+            }
+
+
+            is LoginEvent.OnSenhaChange -> {
+                _uiState.update { state ->
+                    state.copy(
+                        senha = event.value,
+                        errorMessage = null
+                    )
+                }
+            }
+
+
+            LoginEvent.OnEntrarClick -> FazerLogin()
+        }
+    }
+
+
+    private fun FazerLogin() {
+        val state = _uiState.value
+        if(state.usuario.isBlank() || state.senha.isBlank()){
+            _uiState.update { state ->
+                state.copy(
+                    errorMessage = "Preencha login e senha"
+                )
+            }
+            return
+        }
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isLoading = true,
+                    errorMessage = null,
+                    usuarioLogado = null
+                )
+            }
+        }
+        val result = repository.login(state.usuario.trim(),
+            state.senha.trim())
+    }
+}
+
+
+
+
+
+
+
+
+
